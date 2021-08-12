1f4ec1c4aa38ecae34980fdf9c28f1c0907c01ec
Josh Schmitz <joshschmitz16@outlook.com>
2021-08-12 10:54:37 -0500

    Update login
    update path

--- script/login.js
+++ script/login.js
@@ -7,9 +7,9 @@ loginButton.addEventListener("click", (e) => {
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
-    if (username === "ttcorp" && password === "JScriptdumb") {
+    if (username === "ttcorp" && password === "JS") {
         alert("You have successfully logged in.");
-        window.location = "Inventory-Management/pages/selection.html"
+        window.location = "./pages/selection.html"
     } else {
         loginErrorMsg.style.opacity = 1;
     }
